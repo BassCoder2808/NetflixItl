44a995b97dc9afa89448d89b7a42d9d941de1149
BassCoder2808 <vedant.jolly@spit.ac.in>
2021-12-01 19:08:36 +0530

    added build version

---
+++ build/precache-manifest.2b729e3861ccd4d8e1a2bc900e717202.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f99184ef4b7b6c310e916cf61f3c5cb",
+    "url": "/Netflix-ITL/index.html"
+  },
+  {
+    "revision": "c8c2d1d1c60335b7cf66",
+    "url": "/Netflix-ITL/static/css/1.0e7d9586.chunk.css"
+  },
+  {
+    "revision": "43cd9e71f7bd3a6e7874",
+    "url": "/Netflix-ITL/static/css/5.4f6e69d9.chunk.css"
+  },
+  {
+    "revision": "53643012cd78baa3d507",
+    "url": "/Netflix-ITL/static/css/6.4f6e69d9.chunk.css"
+  },
+  {
+    "revision": "0d0bac111a1d7cd8cae2",
+    "url": "/Netflix-ITL/static/css/7.4ec44fee.chunk.css"
+  },
+  {
+    "revision": "018dcbad08fed1b486bf",
+    "url": "/Netflix-ITL/static/css/8.4ec44fee.chunk.css"
+  },
+  {
+    "revision": "b4ba78624e30a4d37e52",
+    "url": "/Netflix-ITL/static/css/main.90e606c0.chunk.css"
+  },
+  {
+    "revision": "17bde4a45f9ef2136c84",
+    "url": "/Netflix-ITL/static/js/0.3dca96ba.chunk.js"
+  },
+  {
+    "revision": "c8c2d1d1c60335b7cf66",
+    "url": "/Netflix-ITL/static/js/1.83d75e0b.chunk.js"
+  },
+  {
+    "revision": "fed559d0da6ab5e9f45f",
+    "url": "/Netflix-ITL/static/js/4.468c632d.chunk.js"
+  },
+  {
+    "revision": "43cd9e71f7bd3a6e7874",
+    "url": "/Netflix-ITL/static/js/5.d76c5102.chunk.js"
+  },
+  {
+    "revision": "53643012cd78baa3d507",
+    "url": "/Netflix-ITL/static/js/6.2c8c73af.chunk.js"
+  },
+  {
+    "revision": "0d0bac111a1d7cd8cae2",
+    "url": "/Netflix-ITL/static/js/7.31f15478.chunk.js"
+  },
+  {
+    "revision": "018dcbad08fed1b486bf",
+    "url": "/Netflix-ITL/static/js/8.767beea7.chunk.js"
+  },
+  {
+    "revision": "b4ba78624e30a4d37e52",
+    "url": "/Netflix-ITL/static/js/main.68efbc84.chunk.js"
+  },
+  {
+    "revision": "9100a1992bb3b452675b",
+    "url": "/Netflix-ITL/static/js/runtime~main.2a0891ef.js"
+  },
+  {
+    "revision": "d6d290478902fbbe09c620b4aaba985f",
+    "url": "/Netflix-ITL/static/media/AIMsqmnV.d6d29047.mp3"
+  },
+  {
+    "revision": "d2edb29f3f970c36aadecbb01ed0bb79",
+    "url": "/Netflix-ITL/static/media/Background.d2edb29f.jpg"
+  },
+  {
+    "revision": "369787c5653cd811d0828096039cc933",
+    "url": "/Netflix-ITL/static/media/cHBfBHfv.369787c5.mp3"
+  },
+  {
+    "revision": "3a36d79cdda4606c11565122b4edd691",
+    "url": "/Netflix-ITL/static/media/cSWrcZcB.3a36d79c.png"
+  },
+  {
+    "revision": "f7f2904f1f3f39d9b1ff2400c30bcecc",
+    "url": "/Netflix-ITL/static/media/imdb.f7f2904f.png"
+  },
+  {
+    "revision": "a1f673e7df715f16dae49f4874009082",
+    "url": "/Netflix-ITL/static/media/logo.a1f673e7.png"
+  },
+  {
+    "revision": "d902c77253bec61f403a5978d2e7c858",
+    "url": "/Netflix-ITL/static/media/netflix.d902c772.jpg"
+  },
+  {
+    "revision": "fd3ce8aa9f2030a21f7ba0724f58ce8c",
+    "url": "/Netflix-ITL/static/media/rsRlAjMp.fd3ce8aa.png"
+  },
+  {
+    "revision": "9179862c82e20d655c43c9b468b96aa4",
+    "url": "/Netflix-ITL/static/media/star.9179862c.png"
+  }
+]);
